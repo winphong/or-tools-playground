dd65c4f952086e2547b96cf6de93b227050c84f6
winphong <winphong@u.nus.edu>
2021-07-13 00:25:18 +0800

    add gap between end of previous and start of next

--- clash-detection-scheduling.py
+++ clash-detection-scheduling.py
@@ -36,9 +36,9 @@ shiftSlots = {
     14: {'shiftSlotId': 14, 'start': 10, 'end': 18, 'hours': 8},
     # overlapping shiftSlots across day
     15: {'shiftSlotId': 15, 'start': 19, 'end': 5, 'hours': 10},
-    16: {'shiftSlotId': 16, 'start': 4, 'end': 14, 'hours': 10},
+    16: {'shiftSlotId': 16, 'start': 13, 'end': 23, 'hours': 10},
     # overlapping shiftSlots across day
-    17: {'shiftSlotId': 17, 'start': 20, 'end': 22, 'hours': 2},
+    17: {'shiftSlotId': 17, 'start': 11, 'end': 21, 'hours': 10},
     # 18: {'shiftSlotId': 18, 'start': 13, 'end': 16, 'hours': 3},
     # 19: {'shiftSlotId': 19, 'start': 2, 'end': 3, 'hours': 1},
     # 20: {'shiftSlotId': 20, 'start': 1, 'end': 5, 'hours': 4},
@@ -118,17 +118,6 @@ def main():
 
     # =======================================================================================
     #
-    # No clashing
-    # [START no_clash]
-    # for n in all_staff:
-    #     for d in all_days:
-    #         for s in all_shifts:
-    #             model.Add((shifts[n, d, s] *
-    #                       len(shifts_data[n][d][s][2])) >= 0)
-    # [END no_clash]
-    #
-    # =======================================================================================
-    #
     # Each shiftslot is assigned to at most one staff in the schedule period.
     # [START at_most_one_staff]
     for d in all_days:
@@ -168,36 +157,35 @@ def main():
                     clash = not (shiftSlots[s]['end'] <= shiftSlots[s1][
                         'start'] or shiftSlots[s]['start'] >= shiftSlots[s1]['end'])
 
-                    if (clash):
-                        model.Add(
-                            (shifts[n, d, s] + shifts[n, d, s1]) * clash != 2)
+                    model.Add(
+                        (shifts[n, d, s] + shifts[n, d, s1]) * clash != 2)
     # [END no_clashing_shiftSlots_within_a_day]
     #
     # ======================================================================================
     #
     # There should not be any clashing shiftSlot across 2 consecutive day
-    # [START no_clashing_shiftSlots_across_day]
+    # There should be atleast a 7 hours gap between end of shift for previous day and start of shift for next day
+    # [START no_clashing_shiftSlots_across_day AND enforce_7_hours_gap_for_shiftSlots_across_day]
     for n in all_staff:
-        for d in range(len(days)-1):
+        for d in all_days:
             for s in days[d]:
-                for s1 in days[d+1]:
+                for s1 in days[(d+1) % 7]:
                     overnight_shiftSlot = shiftSlots[s]['end'] < shiftSlots[s]['start']
 
                     if (overnight_shiftSlot):
                         clashing_across_day = shiftSlots[s]['end'] > shiftSlots[s1]['start']
                         model.Add(
-                            (shifts[n, d, s] + shifts[n, d+1, s1]) * clashing_across_day != 2)
-    # [END no_clashing_shiftSlots_across_day]
-    #
-    # ======================================================================================
-    #
-    # User should not be assigned shiftSlots for 5 consecutive days
-    # [START no_5_consecutive_days]
-    for n in all_staff:
-        for d in all_days:
-            model.Add(sum(shifts[n, (d + d1) % 7, s]
-                      for d1 in range(5) for s in all_shifts) < 5)
-    # [END no_5_consecutive_days]
+                            (shifts[n, d, s] + shifts[n, (d+1) % 7, s1]) * clashing_across_day != 2)
+
+                        less_than_7_hours_gap = (
+                            shiftSlots[s1]['start'] - shiftSlots[s]['end']) < 7
+
+                        # less_than_7_hours_gap is not the deciding factor
+                        model.Add(
+                            (shifts[n, d, s] + shifts[n, (d+1) % 7, s1]) *
+                            less_than_7_hours_gap != 2
+                        )
+    # [END no_clashing_shiftSlots_across_day AND enforce_7_hours_gap_for_shiftSlots_across_day]
     #
     # ======================================================================================
     #
@@ -211,6 +199,7 @@ def main():
     #
     # ======================================================================================
 
+    # TODO: Equal hour
     model.Maximize(
         sum(
             shifts[n, d, s] * shifts_data[n][d][s][0] * n
@@ -238,9 +227,6 @@ def main():
                     if shifts_data[n][d][s][0] == 1:
                         print('Staff', n, 'works shift',
                               s, '-', shifts_data[n][d][s][3], 'hours', shifts_data[n][d][s][1])
-
-                    else:
-                        print("WF")
         print()
 
     # Statistics.
