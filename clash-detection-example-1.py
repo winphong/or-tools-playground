111b3622bd6cf11f4c453711df1e941a772c3ab5
winphong <winphong@u.nus.edu>
2021-07-13 22:59:45 +0800

    add comments, tidy up example

--- clash-detection-example-1.py
+++ clash-detection-example-1.py
@@ -1,62 +1,69 @@
+# This example is based on the simplified shift structure of QA Employment
 from ortools.sat.python import cp_model
 
+num_shiftSlots_per_day = 9  # NOTE: n should be a multiple of 3
 days = {
-    0: range(0, 15),
-    1: range(15, 30),
-    2: range(30, 45),
-    3: range(45, 60),
-    4: range(60, 75),
-    5: range(75, 90),
-    6: range(90, 105),
+    0: range(0, num_shiftSlots_per_day),
+    1: range(num_shiftSlots_per_day, num_shiftSlots_per_day*2),
+    2: range(num_shiftSlots_per_day*2, num_shiftSlots_per_day*3),
+    3: range(num_shiftSlots_per_day*3, num_shiftSlots_per_day*4),
+    4: range(num_shiftSlots_per_day*4, num_shiftSlots_per_day*5),
+    5: range(num_shiftSlots_per_day*5, num_shiftSlots_per_day*6),
+    6: range(num_shiftSlots_per_day*6, num_shiftSlots_per_day*7),
 }
 
 shiftSlots = {}
+# Create num_shiftSlots_per_day number of shift slot which consist of
+# 1/3 of 0700-1500, 1/3 of 1300-2100 and 1/3 of 2100+0700(+1)
 for day in range(len(days)):
     for shiftSlotId in days[day]:
-
-        if (shiftSlotId % 15 < 5):
-            shiftSlots[shiftSlotId] = {'shiftSlotId': shiftSlotId,
-                                       'start': 7,
-                                       'end': 15,
-                                       'hours': 8
-                                       }
+        if (shiftSlotId % num_shiftSlots_per_day < num_shiftSlots_per_day / 3):
+            # TODO: convert start/end/hours to minutes to handle 12:15 / 12:30 shift
+            shiftSlots[shiftSlotId] = {
+                'shiftSlotId': shiftSlotId,
+                'start': 7,
+                'end': 15,
+                'hours': 8
+            }
             continue
 
-        if (shiftSlotId % 15 < 10):
-            shiftSlots[shiftSlotId] = {'shiftSlotId': shiftSlotId,
-                                       'start': 13,
-                                       'end': 21,
-                                       'hours': 8
-                                       }
+        if (shiftSlotId % num_shiftSlots_per_day < num_shiftSlots_per_day / 3 * 2):
+            shiftSlots[shiftSlotId] = {
+                'shiftSlotId': shiftSlotId,
+                'start': 13,
+                'end': 21,
+                'hours': 8
+            }
             continue
 
-        shiftSlots[shiftSlotId] = {'shiftSlotId': shiftSlotId,
-                                   'start': 21,
-                                   'end': 7,
-                                   'hours': 10
-                                   }
+        shiftSlots[shiftSlotId] = {
+            'shiftSlotId': shiftSlotId,
+            'start': 21,
+            'end': 7,
+            'hours': 10
+        }
 
-# assuming ordered by absentism rate in ascending order (worst to best)
+# score can be a weighted score of multiple metrics combined (x out of 100)
 staff_dict = {
-    # to convert to minutes
-    0: {'userId': 'marcus', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    1: {'userId': 'cherry', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    2: {'userId': 'daniel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    3: {'userId': 'elijah', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
-    4: {'userId': 'phillip', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    5: {'userId': 'marcel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    6: {'userId': 'winston', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
-    7: {'userId': 'arial', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    8: {'userId': 'tina', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
-    9: {'userId': 'marcus_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    10: {'userId': 'cherry_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    11: {'userId': 'daniel_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
-    12: {'userId': 'elijah_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    13: {'userId': 'phillip_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    14: {'userId': 'marcel_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    15: {'userId': 'winston_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
-    16: {'userId': 'arial_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    17: {'userId': 'tina_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
+    # TODO: convert daily_ot_limit/weekly_ot_limit to minutes
+    0: {'userId': 'marcus', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 1, 'fulltime': 1},
+    1: {'userId': 'cherry', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 2, 'fulltime': 1},
+    2: {'userId': 'daniel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 3, 'fulltime': 1},
+    3: {'userId': 'elijah', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 4, 'fulltime': 1},
+    4: {'userId': 'phillip', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 5, 'fulltime': 1},
+    5: {'userId': 'marcel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 6, 'fulltime': 1},
+    6: {'userId': 'winston', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 7, 'fulltime': 1},
+    7: {'userId': 'arial', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 8, 'fulltime': 1},
+    8: {'userId': 'tina', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 9, 'fulltime': 1},
+    9: {'userId': 'marcus_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 10, 'fulltime': 0},
+    10: {'userId': 'cherry_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 11, 'fulltime': 0},
+    11: {'userId': 'daniel_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 12, 'fulltime': 0},
+    12: {'userId': 'elijah_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 13, 'fulltime': 0},
+    13: {'userId': 'phillip_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 14, 'fulltime': 0},
+    14: {'userId': 'marcel_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 15, 'fulltime': 0},
+    15: {'userId': 'winston_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 16, 'fulltime': 0},
+    16: {'userId': 'arial_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 17, 'fulltime': 0},
+    17: {'userId': 'tina_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 18, 'fulltime': 0},
 }
 
 shifts_data = []
@@ -98,35 +105,42 @@ def main():
 
     # =======================================================================================
     #
-    # Each shiftslot is assigned to at most one staff in the schedule period.
+    # Constraint 1:
+    # - Each shiftslot is assigned to at most one staff.
     # [START at_most_one_staff]
     for d in all_days:
         for s in all_shifts:
+            # Constraint 1
             model.Add(sum(shifts[(n, d, s)] for n in all_staff) <= 1)
     # [END at_most_one_staff]
     #
     # =======================================================================================
     #
-    # Each staff works at most n hour per week according to weekly OT limit.
+    # Constraint 2:
+    # - Each staff works at most n hour per week according to weekly OT limit.
     # [START at_most_n_weekly_hours]
     for n in all_staff:
+        # Constraint 2
         model.Add(sum(shifts[(n, d, s)] * shifts_data[n][d][s][1]
                   for d in all_days for s in all_shifts) <= staff_dict[n]['weekly_ot_limit'])
     # [END at_most_n_weekly_hours]
     #
     # =======================================================================================
     #
-    # Each staff works at most m hour per day according to daily OT limit.
+    # Constraint 3:
+    # - Each staff works at most m hour per day according to daily OT limit.
     # [START at_most_m_daily_hours]
     for n in all_staff:
         for d in all_days:
+            # Constraint 3
             model.Add(sum(shifts[(n, d, s)] * shifts_data[n][d][s][1]
                           for s in all_shifts) <= staff_dict[n]['daily_ot_limit'])
     # [END at_most_m_daily_hours]
     #
     # =======================================================================================
     #
-    # There should not be any clashing shiftSlot within a day
+    # Constraint 4:
+    # - There should not be any clashing shiftSlot within a day
     # [START no_clashing_shiftSlots_within_a_day]
     for n in all_staff:
         for d in all_days:
@@ -136,15 +150,17 @@ def main():
                         continue
                     clash = not (shiftSlots[s]['end'] <= shiftSlots[s1][
                         'start'] or shiftSlots[s]['start'] >= shiftSlots[s1]['end'])
-
+                    # Constraint 4
                     model.Add(
                         (shifts[n, d, s] + shifts[n, d, s1]) * clash != 2)
     # [END no_clashing_shiftSlots_within_a_day]
     #
     # ======================================================================================
     #
-    # There should not be any clashing shiftSlot across 2 consecutive day
-    # There should be atleast a 7 hours gap between end of shift for previous day and start of shift for next day
+    # Constraint 5:
+    # - There should not be any clashing shiftSlot across 2 consecutive day
+    # Constraint 6:
+    # - There should be atleast a 7 hours gap between end of shift for previous day and start of shift for next day
     # [START no_clashing_shiftSlots_across_day AND enforce_7_hours_gap_for_shiftSlots_across_day]
     for n in all_staff:
         for d in all_days:
@@ -154,12 +170,14 @@ def main():
 
                     if (overnight_shiftSlot):
                         clashing_across_day = shiftSlots[s]['end'] > shiftSlots[s1]['start']
+                        # Constraint 5
                         model.Add(
                             (shifts[n, d, s] + shifts[n, (d+1) % 7, s1]) * clashing_across_day != 2)
 
                         less_than_7_hours_gap = (
                             shiftSlots[s1]['start'] - shiftSlots[s]['end']) < 7
 
+                        # Constraint 6
                         model.Add(
                             (shifts[n, d, s] + shifts[n, (d+1) % 7, s1]) *
                             less_than_7_hours_gap != 2
@@ -168,23 +186,42 @@ def main():
     #
     # ======================================================================================
     #
-    # User should not be assigned shiftSlots for 5 consecutive days
+    # Constraint 7:
+    # - User should not be assigned shiftSlots for 5 consecutive days
     # [START no_5_consecutive_days]
     for n in all_staff:
         for d in all_days:
+            # Constraint 7
             model.Add(sum(shifts[n, (d + d1) % 7, s]
                       for d1 in range(5) for s in all_shifts) < 5)
     # [END no_5_consecutive_days]
     #
     # ======================================================================================
 
-    # == Maximize assignment of shiftSlot to user with higher rating ==
-    # User with highest rating will be sorted to the end of the list
-    # By maximizing the index of user, the algorithm will prioritize assignment to user
-    # with highest index number (better performance)
+    # Objective 1: Maximize assignment of shiftSlot to user with higher score
+    # By maximizing the score of user, the algorithm will prioritize assignment to user
+    # with the highest score
+    # model.Maximize(
+    #     sum(
+    #         shifts[n, d, s] * shifts_data[n][d][s][0] * staff_dict[n]['score']
+    #         # adding 100 to full-timer to give precedence for full-timer over part-timer
+    #         # (staff_dict[n]['score'] + (100 if staff_dict[n]
+    #         #  ['fulltime'] == 1 else 0))
+    #         for n in all_staff
+    #         for d in all_days
+    #         for s in all_shifts
+    #     )
+    # )
+
+    # Objective 2: Maximize assignment of shiftSlot to user with higher score with full-timer prioritization
+    # Similar to Objective 1 but if the user is a full-timer, prioritize the full-timer first
+    # before considering the score.
     model.Maximize(
         sum(
-            shifts[n, d, s] * shifts_data[n][d][s][0] * n
+            shifts[n, d, s] * shifts_data[n][d][s][0] *
+            # adding 100 to full-timer to give precedence for full-timer over part-timer
+            (staff_dict[n]['score'] + (100 if staff_dict[n]
+             ['fulltime'] == 1 else 0))
             for n in all_staff
             for d in all_days
             for s in all_shifts
@@ -201,6 +238,10 @@ def main():
         print(solver.ResponseStats())
         return print()
 
+    output = {}
+    for n in all_staff:
+        output[n] = []
+
     for d in all_days:
         print('Day', d)
         for n in all_staff:
@@ -209,7 +250,11 @@ def main():
                     if shifts_data[n][d][s][0] == 1:
                         print('Staff', n, 'works shift',
                               s, '-', shifts_data[n][d][s][1], 'hours', shifts_data[n][d][s][2])
+                        output[n].append(s)
+
         print()
+
+    print(output)
 
     # Statistics.
     print()
--- clash-detection-scheduling.py
+++ clash-detection-scheduling.py
@@ -1,13 +1,6 @@
 from ortools.sat.python import cp_model
 
 days = {
-    # 0: [0, 1],
-    # 1: [2],
-    # 2: [3],
-    # 3: [4, 5, 6],
-    # 4: [],
-    # 5: [],
-    # 6: [],
     0: [0, 1],
     1: [2, 3],
     2: [4, 5, 6],
@@ -18,6 +11,7 @@ days = {
 }
 
 shiftSlots = {
+    # TODO: convert start/end/hours to minutes to handle 12:15 / 12:30 shift
     0: {'shiftSlotId': 0, 'start': 12, 'end': 20, 'hours': 8},
     1: {'shiftSlotId': 1, 'start': 8, 'end': 16, 'hours': 8},
     2: {'shiftSlotId': 2, 'start': 3, 'end': 5, 'hours': 2},
@@ -42,27 +36,27 @@ shiftSlots = {
     20: {'shiftSlotId': 20, 'start': 1, 'end': 5, 'hours': 4},
 }
 
-# assuming ordered by absentism rate in ascending order (worst to best)
+# score can be a weighted score of multiple metrics combined (x out of 100)
 staff_dict = {
-    # to convert to minutes
-    0: {'userId': 'marcus', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    1: {'userId': 'cherry', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    2: {'userId': 'daniel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    3: {'userId': 'elijah', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    4: {'userId': 'phillip', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    5: {'userId': 'marcel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    6: {'userId': 'winston', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    7: {'userId': 'arial', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    8: {'userId': 'tina', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    9: {'userId': 'marcus_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    10: {'userId': 'cherry_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    11: {'userId': 'daniel_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    12: {'userId': 'elijah_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    13: {'userId': 'phillip_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    14: {'userId': 'marcel_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46},
-    15: {'userId': 'winston_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
-    16: {'userId': 'arial_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44},
-    17: {'userId': 'tina_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46},
+    # TODO: convert daily_ot_limit/weekly_ot_limit to minutes
+    0: {'userId': 'marcus', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 1, 'fulltime': 1},
+    1: {'userId': 'cherry', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 2, 'fulltime': 1},
+    2: {'userId': 'daniel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 3, 'fulltime': 1},
+    3: {'userId': 'elijah', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 4, 'fulltime': 1},
+    4: {'userId': 'phillip', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 5, 'fulltime': 1},
+    5: {'userId': 'marcel', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 6, 'fulltime': 1},
+    6: {'userId': 'winston', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 7, 'fulltime': 1},
+    7: {'userId': 'arial', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 8, 'fulltime': 1},
+    8: {'userId': 'tina', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 9, 'fulltime': 1},
+    9: {'userId': 'marcus_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 10, 'fulltime': 0},
+    10: {'userId': 'cherry_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 11, 'fulltime': 0},
+    11: {'userId': 'daniel_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 12, 'fulltime': 0},
+    12: {'userId': 'elijah_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 13, 'fulltime': 0},
+    13: {'userId': 'phillip_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 14, 'fulltime': 0},
+    14: {'userId': 'marcel_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 46, 'score': 15, 'fulltime': 0},
+    15: {'userId': 'winston_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 16, 'fulltime': 0},
+    16: {'userId': 'arial_1', 'daily_ot_limit': 8, 'weekly_ot_limit': 44, 'score': 17, 'fulltime': 0},
+    17: {'userId': 'tina_1', 'daily_ot_limit': 10, 'weekly_ot_limit': 46, 'score': 18, 'fulltime': 0},
 }
 
 shifts_data = []
@@ -104,35 +98,42 @@ def main():
 
     # =======================================================================================
     #
-    # Each shiftslot is assigned to at most one staff in the schedule period.
+    # Constraint 1:
+    # - Each shiftslot is assigned to at most one staff.
     # [START at_most_one_staff]
     for d in all_days:
         for s in all_shifts:
+            # Constraint 1
             model.Add(sum(shifts[(n, d, s)] for n in all_staff) <= 1)
     # [END at_most_one_staff]
     #
     # =======================================================================================
     #
-    # Each staff works at most n hour per week according to weekly OT limit.
+    # Constraint 2:
+    # - Each staff works at most n hour per week according to weekly OT limit.
     # [START at_most_n_weekly_hours]
     for n in all_staff:
+        # Constraint 2
         model.Add(sum(shifts[(n, d, s)] * shifts_data[n][d][s][1]
                   for d in all_days for s in all_shifts) <= staff_dict[n]['weekly_ot_limit'])
     # [END at_most_n_weekly_hours]
     #
     # =======================================================================================
     #
-    # Each staff works at most m hour per day according to daily OT limit.
+    # Constraint 3:
+    # - Each staff works at most m hour per day according to daily OT limit.
     # [START at_most_m_daily_hours]
     for n in all_staff:
         for d in all_days:
+            # Constraint 3
             model.Add(sum(shifts[(n, d, s)] * shifts_data[n][d][s][1]
                           for s in all_shifts) <= staff_dict[n]['daily_ot_limit'])
     # [END at_most_m_daily_hours]
     #
     # =======================================================================================
     #
-    # There should not be any clashing shiftSlot within a day
+    # Constraint 4:
+    # - There should not be any clashing shiftSlot within a day
     # [START no_clashing_shiftSlots_within_a_day]
     for n in all_staff:
         for d in all_days:
@@ -142,15 +143,17 @@ def main():
                         continue
                     clash = not (shiftSlots[s]['end'] <= shiftSlots[s1][
                         'start'] or shiftSlots[s]['start'] >= shiftSlots[s1]['end'])
-
+                    # Constraint 4
                     model.Add(
                         (shifts[n, d, s] + shifts[n, d, s1]) * clash != 2)
     # [END no_clashing_shiftSlots_within_a_day]
     #
     # ======================================================================================
     #
-    # There should not be any clashing shiftSlot across 2 consecutive day
-    # There should be atleast a 7 hours gap between end of shift for previous day and start of shift for next day
+    # Constraint 5:
+    # - There should not be any clashing shiftSlot across 2 consecutive day
+    # Constraint 6:
+    # - There should be atleast a 7 hours gap between end of shift for previous day and start of shift for next day
     # [START no_clashing_shiftSlots_across_day AND enforce_7_hours_gap_for_shiftSlots_across_day]
     for n in all_staff:
         for d in all_days:
@@ -160,12 +163,14 @@ def main():
 
                     if (overnight_shiftSlot):
                         clashing_across_day = shiftSlots[s]['end'] > shiftSlots[s1]['start']
+                        # Constraint 5
                         model.Add(
                             (shifts[n, d, s] + shifts[n, (d+1) % 7, s1]) * clashing_across_day != 2)
 
                         less_than_7_hours_gap = (
                             shiftSlots[s1]['start'] - shiftSlots[s]['end']) < 7
 
+                        # Constraint 6
                         model.Add(
                             (shifts[n, d, s] + shifts[n, (d+1) % 7, s1]) *
                             less_than_7_hours_gap != 2
@@ -174,30 +179,47 @@ def main():
     #
     # ======================================================================================
     #
-    # User should not be assigned shiftSlots for 5 consecutive days
+    # Constraint 7:
+    # - User should not be assigned shiftSlots for 5 consecutive days
     # [START no_5_consecutive_days]
     for n in all_staff:
         for d in all_days:
+            # Constraint 7
             model.Add(sum(shifts[n, (d + d1) % 7, s]
                       for d1 in range(5) for s in all_shifts) < 5)
     # [END no_5_consecutive_days]
     #
     # ======================================================================================
 
-    # TODO: Equal hour
-
-    # == Maximize assignment of shiftSlot to user with higher rating ==
-    # User with highest rating will be sorted to the end of the list
-    # By maximizing the index of user, the algorithm will prioritize assignment to user
-    # with highest index number (better performance)
+    # Objective 1: Maximize assignment of shiftSlot to user with higher score
+    # By maximizing the score of user, the algorithm will prioritize assignment to user
+    # with the highest score
     model.Maximize(
         sum(
-            shifts[n, d, s] * shifts_data[n][d][s][0] * n
+            shifts[n, d, s] * shifts_data[n][d][s][0] * staff_dict[n]['score']
+            # adding 100 to full-timer to give precedence for full-timer over part-timer
+            # (staff_dict[n]['score'] + (100 if staff_dict[n]
+            #  ['fulltime'] == 1 else 0))
             for n in all_staff
             for d in all_days
             for s in all_shifts
         )
     )
+
+    # Objective 2: Maximize assignment of shiftSlot to user with higher score with full-timer prioritization
+    # Similar to Objective 1 but if the user is a full-timer, prioritize the full-timer first
+    # before considering the score.
+    # model.Maximize(
+    #     sum(
+    #         shifts[n, d, s] * shifts_data[n][d][s][0]
+    #         # adding 100 to full-timer to give precedence for full-timer over part-timer
+    #         (staff_dict[n]['score'] + (100 if staff_dict[n]
+    #          ['fulltime'] == 1 else 0))
+    #         for n in all_staff
+    #         for d in all_days
+    #         for s in all_shifts
+    #     )
+    # )
 
     # Creates the solver and solve.
     solver = cp_model.CpSolver()
@@ -209,6 +231,10 @@ def main():
         print(solver.ResponseStats())
         return print()
 
+    output = {}
+    for n in all_staff:
+        output[n] = []
+
     for d in all_days:
         print('Day', d)
         for n in all_staff:
@@ -217,7 +243,11 @@ def main():
                     if shifts_data[n][d][s][0] == 1:
                         print('Staff', n, 'works shift',
                               s, '-', shifts_data[n][d][s][1], 'hours', shifts_data[n][d][s][2])
+                        output[n].append(s)
+
         print()
+
+    print(output)
 
     # Statistics.
     print()
